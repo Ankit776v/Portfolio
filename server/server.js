9423f3c42e0064777d43234d957d1ae7ae954008
Akki776v <ankit786v@gmail.com>
2026-04-04 22:20:59 +0530

    Fix routing order2

--- server/server.js
+++ server/server.js
@@ -54,7 +54,7 @@ app.post("/contact", async (req, res) => {
 app.use(express.static(path.join(__dirname, "../dist")));
 
 // React fallback (MUST BE LAST)
-app.get("/*", (req, res) => {
+app.get((req, res) => {
   res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
 
